23d26e19fd9920e338fd22582caa8db2ceb00f36
BenWhitehead <BenWhitehead@users.noreply.github.com>
2020-06-30 14:30:41 -0400

    Bulk File update
    * Add 'Hack' profiles
    * Update templates to all be prefixed for easier identifiaction

--- config/fileTemplates/internal/Bash Script.sh
+++ config/fileTemplates/internal/Bash Script.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+set -o errexit -o nounset -o pipefail
+
+__SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function main() { ( 
+
+) }
+
+function now { date +"%Y-%m-%d %H:%M:%S" | tr -d '\n' ;}
+function msg { println "$*" >&2 ;}
+function err { local x=$? ; msg "$*" ; return $(( $x == 0 ? 1 : $x )) ;}
+function println { printf '%s\n' "$(now) $*" ;}
+function print { printf '%s ' "$(now) $*" ;}
+
+######################### Delegates to subcommands or runs main, as appropriate
+if [[ ${1:-} ]] && declare -F | cut -d' ' -f3 | fgrep -qx -- "${1:-}"
+then "$@"
+else main "$@"
+fi
